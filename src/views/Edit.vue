f8a3e350a3a49831c1809e940f87bf7c440bd256
Nick Andreev <bakasaru@list.ru>
2019-04-28 22:31:08 +0300

    Add Marked package and Edit view

---
+++ src/views/Edit.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-desktop">
+        <div class="column one-half">
+          <div class="field">
+            <div class="control">
+              <textarea class="textarea" id="source" v-model="text"></textarea>
+            </div>
+          </div>
+        </div>
+        <div class="column one-half">
+          <div class="box">
+            <div class="content" v-html="compiledMarkdown"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import marked from 'marked';
+
+export default {
+  name: 'Editor',
+  data: () => ({
+    text: '# Hello World!',
+  }),
+
+  computed: {
+    compiledMarkdown() {
+      return marked(this.text, { sanitize: true });
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .textarea {
+    height: 100vh;
+  }
+</style>
