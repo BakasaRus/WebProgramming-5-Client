28371de02f9bd49ede7d1ee95da2875a8394162b
Nick Andreev <bakasaru@list.ru>
2019-04-28 22:32:50 +0300

    Remove navbar and change DocCard a bit

--- src/components/DocCard.vue
+++ src/components/DocCard.vue
@@ -11,7 +11,6 @@
       </div>
     </div>
     <footer class="card-footer">
-      <a href="#" class="card-footer-item">Save</a>
       <a href="#" class="card-footer-item">Edit</a>
       <a href="#" class="card-footer-item">Delete</a>
     </footer>
