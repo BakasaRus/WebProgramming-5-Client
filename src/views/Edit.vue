994840a4d34ca912c379154822deba869e3ce2bf
Nick Andreev <bakasaru@list.ru>
2019-04-28 22:51:56 +0300

    Cool styling which won't give me any points

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,20 +1,23 @@
 <template>
   <section class="section">
     <div class="container">
+      <div class="is-flex control-panel">
+        <h2 class="title is-2">{{ title }}</h2>
+        <div class="field is-grouped">
+          <p class="control">
+            <button class="button is-success">Сохранить</button>
+          </p>
+          <p class="control">
+            <button class="button is-danger">Удалить</button>
+          </p>
+        </div>
+      </div>
       <div class="columns is-desktop">
         <div class="column one-half">
           <div class="field">
             <div class="control">
-              <textarea class="textarea" id="source" v-model="text"></textarea>
+              <textarea class="textarea" id="source" v-model="body"></textarea>
             </div>
-          </div>
-          <div class="field is-grouped">
-            <p class="control">
-              <button class="button is-success">Сохранить</button>
-            </p>
-            <p class="control">
-              <button class="button is-danger">Удалить</button>
-            </p>
           </div>
         </div>
         <div class="column one-half">
@@ -33,12 +36,13 @@ import marked from 'marked';
 export default {
   name: 'Editor',
   data: () => ({
-    text: '# Hello World!',
+    title: 'New Markdown document',
+    body: '# Hello World!',
   }),
 
   computed: {
     compiledMarkdown() {
-      return marked(this.text, { sanitize: true });
+      return marked(this.body, { sanitize: true });
     },
   },
 };
@@ -47,5 +51,10 @@ export default {
 <style scoped>
   .textarea {
     height: 100vh;
+  }
+
+  .control-panel {
+    justify-content: space-between;
+    align-content: center;
   }
 </style>
