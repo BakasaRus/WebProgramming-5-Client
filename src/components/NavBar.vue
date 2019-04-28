3ddb2f97d6265aadb806ca380e7ec89c7eea54fe
Nick Andreev <bakasaru@list.ru>
2019-04-29 00:58:23 +0300

    Extract navbar to separate component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="navbar has-shadow is-spaced" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <router-link class="navbar-item" to="/">
+          <h3 class="title is-3">MD Notes</h3>
+        </router-link>
+
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu">
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <router-link to="/create" class="button is-primary">
+              Новый документ
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
