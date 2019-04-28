8fbae8dd12effd5a5e823945bb0c683fcc049f6a
Nick Andreev <bakasaru@list.ru>
2019-04-29 01:26:41 +0300

    Update navbar a bit

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,19 @@
 <template>
   <nav class="navbar has-shadow is-spaced" role="navigation" aria-label="main navigation">
-    <div class="container">
       <div class="navbar-brand">
         <router-link class="navbar-item" to="/">
           <h3 class="title is-3">MD Notes</h3>
         </router-link>
 
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false">
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+          data-target="appNavbar">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div class="navbar-menu">
-
+      <div id="appNavbar" class="navbar-menu">
         <div class="navbar-end">
           <div class="navbar-item">
             <router-link to="/create" class="button is-primary">
@@ -23,6 +22,5 @@
           </div>
         </div>
       </div>
-    </div>
   </nav>
 </template>
